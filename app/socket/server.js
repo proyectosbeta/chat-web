5ef4b038c874707d0ef118fcc81938cce155dfd4
josego85 <josego85@gmail.com>
2022-07-02 18:19:14 +0200

    [fix] Change port

--- app/socket/server.js
+++ app/socket/server.js
@@ -4,7 +4,7 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-const PORT = 3000;
+const PORT = 5000;
 
 app.use(express.static('public'));
 
